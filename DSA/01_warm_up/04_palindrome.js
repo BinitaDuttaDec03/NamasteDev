c608a595c706edd8fe2e037c9d132c55ca230e1e
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-03 03:36:42 +0530

    Day 1 - Chapter Warm Up - Palindrome

---
+++ DSA/01_warm_up/04_palindrome.js
@@ -0,0 +1,30 @@
+/* Problem Statement:
+Write a function isPalindrome(x) that takes an integer x and returns true if it reads the same backward and forward; otherwise false.
+
+Requirements:
+Handles both positive and negative integers.
+Return false for negative numbers (not Palindromes).
+Constraints:
+Time Complexity: O(d)Where dis the number of digits.
+
+Space Complexity: O(1)Only a few variables are used. */
+
+// const num = 1221;
+// const num = 1222;
+const num = -1222;
+
+function isPalindrome(x) {
+  if (x < 0) return false;
+  let num = x,
+    rev = 0;
+
+  while (num > 0) {
+    rev = rev * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  if (rev === x) return true;
+  return false;
+}
+
+console.log(isPalindrome(num));
