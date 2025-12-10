7ed10155819bd15d243a994127223fb68f197242
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-10 06:00:07 +0530

    Day 6 - Chapter Arrays - Merge sorted arrays

--- DSA/02_arrays (easy_medium)/05_merge_sorted_arrays.js
+++ DSA/02_arrays (easy_medium)/05_merge_sorted_arrays.js
@@ -53,6 +53,44 @@ Explanation: The arrays we are merging are [] and [1]. The result of the merge i
 // console.log(nums1);
 
 // Approach - 2
+// let nums1 = [1, 2, 3, 0, 0, 0],
+//   m = 3;
+// let nums2 = [2, 5, 6],
+//   n = 3;
+
+// let nums1 = [1],
+//   m = 1;
+// let nums2 = [],
+//   n = 0;
+
+// let nums1 = [0],
+//   m = 0;
+// let nums2 = [1],
+//   n = 1;
+
+// let nums1 = [2, 0],
+//   m = 1;
+// let nums2 = [1],
+//   n = 1;
+
+// function sortedArray(nums1, m, nums2, n) {
+//   let nums1Copy = nums1.slice(0, m);
+//   let p1 = 0,
+//     p2 = 0;
+
+//   for (let i = 0; i < m + n; i++) {
+//     if (p2 >= n || (p1 < m && nums1Copy[p1] <= nums2[p2])) {
+//       nums1[i] = nums1Copy[p1++];
+//     } else {
+//       nums1[i] = nums2[p2++];
+//     }
+//   }
+// }
+
+// sortedArray(nums1, m, nums2, n);
+// console.log(nums1);
+
+// Approach - 3
 let nums1 = [1, 2, 3, 0, 0, 0],
   m = 3;
 let nums2 = [2, 5, 6],
@@ -73,7 +111,20 @@ let nums2 = [2, 5, 6],
 // let nums2 = [1],
 //   n = 1;
 
-function sortedArray(nums1, m, nums2, n) {}
+function sortedArray(nums1, m, nums2, n) {
+  let p1 = m - 1,
+    p2 = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) break;
+
+    if (p1 >= 0 && nums1[p1] >= nums2[p2]) {
+      nums1[i] = nums1[p1--];
+    } else {
+      nums1[i] = nums2[p2--];
+    }
+  }
+}
 
 sortedArray(nums1, m, nums2, n);
 console.log(nums1);
