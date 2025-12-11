3588b941313c84fe2e075126a66ded50cbb22078
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-11 06:30:14 +0530

    Day 7 - Chapter Arrays - Find the missing number

---
+++ DSA/02_arrays (easy_medium)/08_missing_number.js
@@ -0,0 +1,73 @@
+/* Problem Statement: (Leetcode - 268)
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Examples:
+Example 1:
+Input: nums = [3,0,1]
+
+Output: 2
+
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+
+Output: 2
+
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+
+Output: 8
+
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+Constraints:
+n == nums.length
+1 <= n <= 10 4
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+ */
+
+// Approach - 1
+// // const nums = [3, 0, 1];
+// // const nums = [0, 1];
+// const nums = [1, 2];
+
+// function missingNumber(nums) {
+//   nums.sort((a, b) => a - b);
+
+//   let missing = -1;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== i) {
+//       missing = i;
+//       break;
+//     }
+//   }
+
+//   return (missing = missing === -1 ? nums.length : missing);
+// }
+
+// const result = missingNumber(nums);
+// console.log(result);
+
+// Approach - 2
+// const nums = [3, 0, 1];
+const nums = [0, 1];
+// const nums = [1, 2];
+
+function missingNumber(nums) {
+  const n = nums.length;
+  const totalSum = (n * (n + 1)) / 2;
+
+  let partialSum = 0;
+  for (let i = 0; i < n; i++) {
+    partialSum += nums[i];
+  }
+
+  return totalSum - partialSum;
+}
+
+const result = missingNumber(nums);
+console.log(result);
