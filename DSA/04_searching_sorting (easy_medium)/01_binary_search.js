007f844ab0ffa8b9172dbfe2f47a427e26c50cc9
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-13 08:07:42 +0530

    Day 10 - Chapter Searching & Sorting - Binary search

---
+++ DSA/04_searching_sorting (easy_medium)/01_binary_search.js
@@ -0,0 +1,55 @@
+/* Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order. */
+
+// const nums = [-1, 0, 3, 5, 9, 12],
+//   target = 9;
+const nums = [-1, 0, 3, 5, 9, 12],
+  target = 2;
+
+function search(nums, target) {
+  const n = nums.length;
+  if (n === 1) {
+    if (nums[0] === target) return 0;
+    return -1;
+  }
+
+  let left = 0,
+    right = n - 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+const result = search(nums, target);
+console.log(result);
