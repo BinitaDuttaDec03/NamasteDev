9ef701a87288fbc1a6b9c3e0cd36775aa5a67131
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-14 09:30:20 +0530

    Day 11 - Chapter Searching & Sorting - Selection sort

---
+++ DSA/04_searching_sorting (easy_medium)/03_selection_sort.js
@@ -0,0 +1,20 @@
+const nums = [4, 5, 1, 3, 9];
+
+function sort(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIdx = i;
+
+    for (let j = i + 1; j < n - 1; j++) {
+      if (nums[j] < nums[minIdx]) {
+        minIdx = j;
+      }
+    }
+
+    if (minIdx != i) [nums[i], nums[minIdx]] = [nums[minIdx], nums[i]];
+  }
+}
+
+sort(nums);
+console.log(nums);
