842bfbc9d3c7a81bc6f3a3d92eff10dea271e49e
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-21 09:01:02 +0530

    Day 16 - Chapter Linked List - Remove nth node from end of LL (two pass)

---
+++ DSA/05_linked_list (easy_medium)/11_remove_nth_node_from_end_two_pass.js
@@ -0,0 +1,50 @@
+/* Problem Statement (Leetcode - 19)
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+function removeNthFromEnd(head, n) {
+  let size = 0;
+  let curr = head;
+
+  while (curr) {
+    size++;
+    curr = curr.next;
+  }
+
+  let sentinel = new ListNode();
+  sentinel.next = head;
+
+  let prev = sentinel;
+  let idxBeforeDeletingNode = size - n;
+
+  for (let i = 0; i < idxBeforeDeletingNode; i++) {
+    prev = prev.next;
+  }
+
+  prev.next = prev.next?.next;
+  return sentinel.next;
+}
