a020ea9827ce97bb58b3400e5891eb0ced29b9a4
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-22 07:05:58 +0530

    tracker added

--- DSA/05_linked_list (easy_medium)/08_palindrome.js
+++ DSA/05_linked_list (easy_medium)/08_palindrome.js
@@ -1,4 +1,5 @@
-/* Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+/* Problem Statement (Leetcode - 234)
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 
 Example 1:
 
