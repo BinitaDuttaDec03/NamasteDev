f4b4f14ab088025890cfd92d3e3f9d84331e76a4
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-01-06 06:42:55 +0530

    Day 24 - Chapter Strings - Find the max frequency of vowels and consonants

---
+++ DSA/06_strings (easy_medium)/05_find_most_frequent_vowel_and_consonant.js
@@ -0,0 +1,102 @@
+/* Problem Statement (Leetcode - 3541)
+You are given a string s consisting of lowercase English letters ('a' to 'z').
+
+Your task is to:
+
+Find the vowel (one of 'a', 'e', 'i', 'o', or 'u') with the maximum frequency.
+Find the consonant (all other letters excluding vowels) with the maximum frequency.
+Return the sum of the two frequencies.
+
+Note: If multiple vowels or consonants have the same maximum frequency, you may choose any one of them. If there are no vowels or no consonants in the string, consider their frequency as 0.
+
+The frequency of a letter x is the number of times it occurs in the string.
+
+Example 1:
+
+Input: s = "successes"
+
+Output: 6
+
+Explanation:
+
+The vowels are: 'u' (frequency 1), 'e' (frequency 2). The maximum frequency is 2.
+The consonants are: 's' (frequency 4), 'c' (frequency 2). The maximum frequency is 4.
+The output is 2 + 4 = 6.
+Example 2:
+
+Input: s = "aeiaeia"
+
+Output: 3
+
+Explanation:
+
+The vowels are: 'a' (frequency 3), 'e' ( frequency 2), 'i' (frequency 2). The maximum frequency is 3.
+There are no consonants in s. Hence, maximum consonant frequency = 0.
+The output is 3 + 0 = 3.
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters only. */
+
+// Approach - 1
+// // let s = "successes";
+// let s = "aeiaeia";
+
+// function maxFreqSum(s) {
+//   let map = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     map[s[i]] = map[s[i]] ? ++map[s[i]] : 1;
+//   }
+
+//   let maxVCount = 0,
+//     maxCCount = 0;
+
+//   let vowels = ["a", "e", "i", "o", "u"];
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (vowels.includes(s[i])) {
+//       maxVCount = Math.max(maxVCount, map[s[i]]);
+//     } else {
+//       maxCCount = Math.max(maxCCount, map[s[i]]);
+//     }
+//   }
+
+//   return maxVCount + maxCCount;
+// }
+
+// const result = maxFreqSum(s);
+// console.log(result);
+
+// Approach - 2
+let s = "successes";
+// let s = "aeiaeia";
+
+function maxFreqSum(s) {
+  let map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    map[s[i]] = map[s[i]] ? ++map[s[i]] : 1;
+  }
+
+  let maxVCount = 0,
+    maxCCount = 0;
+
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  let mapKeys = Object.keys(map);
+
+  for (let i = 0; i < mapKeys.length; i++) {
+    if (vowels.includes(mapKeys[i])) {
+      maxVCount = Math.max(maxVCount, map[mapKeys[i]]);
+    } else {
+      maxCCount = Math.max(maxCCount, map[mapKeys[i]]);
+    }
+  }
+
+  return maxVCount + maxCCount;
+}
+
+const result = maxFreqSum(s);
+console.log(result);
