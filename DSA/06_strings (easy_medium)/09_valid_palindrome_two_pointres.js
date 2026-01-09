19a1836650dc8d4827646b99eda093d246b6197e
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-01-09 07:04:42 +0530

    Day 27 - Chapter Strings - Valid palindrome using two pointers

---
+++ DSA/06_strings (easy_medium)/09_valid_palindrome_two_pointres.js
@@ -0,0 +1,53 @@
+/* Problem Statement (leetcode - 125)
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters. */
+
+// let s = "A man, a plan, a canal: Panama";
+// let s = "race a car";
+let s = " ";
+
+function isPalindrome(s) {
+  s = s.toLowerCase();
+
+  let i = 0,
+    j = s.length - 1;
+
+  while (i < j) {
+    if (!s[i].match(/[a-z0-9]/i)) i++;
+    else if (!s[j].match(/[a-z0-9]/i)) j--;
+    else if (s[i] === s[j]) {
+      i++;
+      j--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const result = isPalindrome(s);
+console.log(result);
