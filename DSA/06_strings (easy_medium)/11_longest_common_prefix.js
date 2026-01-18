9cc8b55819cfbfaaf52967b0b233b0a4477cf295
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-01-18 08:57:59 +0530

    Day 29 - Chapter Strings - Longest Common Prefix

--- DSA/06_strings (easy_medium)/11_longest_common_prefix.js
+++ DSA/06_strings (easy_medium)/11_longest_common_prefix.js
@@ -22,10 +22,27 @@ strs[i] consists of only lowercase English letters if it is non-empty. */
 // let strs = ["flower", "flow", "flight"];
 // let strs = ["dog", "racecar", "car"];
 // let strs = ["a"];
-// let strs = ["flower", "flower", "flower", "flower"];
-let strs = ["a", "a", "b"];
+let strs = ["flower", "flower", "flower", "flower"];
 
-function longestCommonPrefix(strs) {}
+function longestCommonPrefix(strs) {
+  if (strs.length === 1) return strs[0];
+
+  let x = 0;
+
+  while (x < strs[0].length) {
+    let ch = strs[0][x];
+
+    for (let i = 1; i < strs.length; i++) {
+      if (strs[i][x] !== ch || x === strs[i].length) {
+        return strs[0].substring(0, x);
+      }
+    }
+
+    x++;
+  }
+
+  return strs[0];
+}
 
 const result = longestCommonPrefix(strs);
 console.log(result);
