468e6c1a2a0be913c4c3113e96e6503b53d3b616
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-02-27 14:46:31 +0530

    Updated readme.txt

--- DSA/04_searching_sorting (easy_medium)/02_bubble_sort.js
+++ DSA/04_searching_sorting (easy_medium)/02_bubble_sort.js
@@ -1,5 +1,24 @@
 const nums = [4, 5, 1, 3, 9];
 
+// Approach - 1
+// function sort(nums) {
+//   let n = arr.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+// }
+
+// sort(nums);
+// console.log(nums);
+
+// Approach - 2
 function sort(nums) {
   const n = nums.length;
 
--- DSA/readme.txt
+++ DSA/readme.txt
@@ -1,4 +1,7 @@
 // TODO:
 1) 01_warm_up - 05
 2) 02_arrays (easy_medium) - 09
-3) 03_recursion (easy_medium) - 06
+3) 03_recursion (easy_medium) - 07
+4) 04_searching_sorting (easy_medium) - 05
+
+check on insertion sort again
