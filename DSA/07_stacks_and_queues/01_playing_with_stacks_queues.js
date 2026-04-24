7de9917902d9781278ddea9484971181a543f7d6
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-04-24 10:07:47 +0530

    Day 36 - Chapter Stacks and Queues - Playing with stacks and queues

---
+++ DSA/07_stacks_and_queues/01_playing_with_stacks_queues.js
@@ -0,0 +1,28 @@
+// stack implementation
+// let stack = []
+
+// // push
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
+
+// // pop
+// console.log(stack.pop())
+
+// // top/peek
+// console.log(stack[stack.length - 1])
+
+
+// queue implementation
+let queue = []
+
+// enqueue
+queue.push(1)
+queue.push(2)
+queue.push(3)
+
+// dequeue
+console.log(queue.shift())
+
+// front
+console.log(queue[0])
