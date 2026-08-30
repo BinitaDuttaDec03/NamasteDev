669ea44bc52e7b635b38fa89471d3f7a6220f258
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-08-30 10:03:32 +0530

    Day 45 - Chapter Binary Search Algorithm - square root of x

--- DSA/08_binary_search_algo/01_square_root_of_x.js
+++ DSA/08_binary_search_algo/01_square_root_of_x.js
@@ -32,9 +32,32 @@
 
 
 // Approach - 2
-var mySqrt = function (x) {
+// var mySqrt = function (x) {
+//     for (let i = 0; i <= x; i++) {
+//         if (i * i === x) return i;
+//         else if (i * i > x) return i - 1;
+//     }
+// };
 
+// const x = 8
+// console.log(mySqrt(x))
+
+
+// Approach - 3
+var mySqrt = function (x) {
+    if (x < 2) return x;
+
+    let l = 2, r = Math.floor(x / 2);
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+
+        if (x === mid * mid) return mid;
+        else if (x < mid * mid) r = mid - 1;
+        else l = mid + 1;
+    }
+
+    return r;
 };
 
-const x = 8
+const x = 40
 console.log(mySqrt(x))
