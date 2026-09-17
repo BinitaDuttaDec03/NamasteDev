2d6298549519ead1c81e0b4bbb861c00ccf6569f
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-17 17:19:28 +0530

    Day 60 - Chapter Two Pointers and Sliding Window - three sum

---
+++ DSA/08_binary_search_algo/11_three_sum.js
@@ -0,0 +1,63 @@
+/* Problem statement (Leetcode - 15)
+    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums [k] == 0.
+
+    Notice that the solution set must not contain duplicate triplets.
+
+    Example 1:
+
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+    Explanation: 
+    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+    The distinct triplets are [-1,0,1] and [-1,-1,2].
+    Notice that the order of the output and the order of the triplets does not matter.
+    Example 2:
+
+    Input: nums = [0,1,1]
+    Output: []
+    Explanation: The only possible triplet does not sum up to 0.
+    Example 3:
+
+    Input: nums = [0,0,0]
+    Output: [[0,0,0]]
+    Explanation: The only possible triplet sums up to 0.
+*/
+
+var threeSum = function (a) {
+    let res = [];
+    let n = a.length;
+    a = a.sort((a, b) => (a - b));
+
+    for (let i = 0; i < n - 2; i++) {
+        if (i === 0 || a[i] !== a[i - 1]) {
+            twoSum(a, i, res);
+        }
+    }
+
+    return res;
+};
+
+function twoSum(a, k, res) {
+    let i = k + 1, j = a.length - 1;
+
+    while (i < j) {
+        let sum = a[k] + a[i] + a[j];
+
+        if (sum < 0) {
+            i++;
+        } else if (sum > 0) {
+            j--;
+        } else {
+            res.push([a[k], a[i], a[j]]);
+
+            while (i < j && a[i] === a[i + 1]) i++;
+
+            i++;
+            j--;
+        }
+    }
+
+    return res;
+}
