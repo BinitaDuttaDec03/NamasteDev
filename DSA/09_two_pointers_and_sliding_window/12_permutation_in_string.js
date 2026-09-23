37afada556fb0321a7907752c3ec29d7dd7795b5
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-09-24 00:09:00 +0530

    Day 64 - Chapter Two Pointers and Sliding Window - permutation in string

--- DSA/09_two_pointers_and_sliding_window/12_permutation_in_string.js
+++ DSA/09_two_pointers_and_sliding_window/12_permutation_in_string.js
@@ -19,20 +19,56 @@
     s1 and s2 consist of lowercase English letters.
 */
 
+// Approach - 1
+// var checkInclusion = function (s1, s2) {
+//     s1 = s1.split("").sort().join("");
+
+//     let i = 0, j = s1.length - 1;
+
+//     while (j < s2.length) {
+//         let window = s2.substring(i, j + 1);
+//         window = window.split("").sort().join("");
+
+//         if (window === s1) return true;
+
+//         i++;
+//         j++;
+//     }
+
+//     return false;
+// };
+
+
+// Approach - 2
 var checkInclusion = function (s1, s2) {
-    s1 = s1.split("").sort().join("");
+    let s1Hash = Array(26).fill(0);
+    let s2Hash = Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        s1Hash[s1.charCodeAt(i) - 97]++;
+        s2Hash[s2.charCodeAt(i) - 97]++;
+    }
 
     let i = 0, j = s1.length - 1;
 
     while (j < s2.length) {
-        let window = s2.substring(i, j + 1);
-        window = window.split("").sort().join("");
+        if (areHashesSame(s1Hash, s2Hash)) {
+            return true;
+        }
 
-        if (window === s1) return true;
-
+        s2Hash[s2.charCodeAt(i) - 97]--;
         i++;
         j++;
+        s2Hash[s2.charCodeAt(j) - 97]++;
     }
 
     return false;
 };
+
+function areHashesSame(s1Hash, s2Hash) {
+    for (let i = 0; i < 26; i++) {
+        if (s1Hash[i] !== s2Hash[i]) return false;
+    }
+
+    return true;
+}
